8d6516a60be07b50d3abbb4c47a87e323ec536bd
Shauray <shauray9@gmail.com>
2022-05-23 14:49:35 +0530

    proly getting the thing off of pandas

--- Trachea/utils.py
+++ Trachea/utils.py
@@ -1,1 +1,6 @@
-import numpy
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+    readme = pd.read_csv("E:/data/LJSpeech-1.1/metadata.csv", header=None)
+    print(readme.head()[1])
